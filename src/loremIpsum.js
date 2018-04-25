28c03dba8827f4e9b88c9e4271145538ea41402b
Marek Tkaczyk <terverok@gmail.com>
2018-04-25 02:40:52 +0200

    add temporary loremIpsum

---
+++ src/loremIpsum.js
@@ -0,0 +1,11 @@
+export default `
+Lorem ipsum dolor sit amet, delicata deseruisse adipiscing ei has, utamur tibique eum ex, eu aeterno tacimates disputando vis. Sea ne suas invenire repudiandae. No sumo mentitum conclusionemque vix. Vocent tacimates honestatis an has. Scripta repudiandae in per, ut ius tacimates disputando neglegentur, id soleat perfecto vim.
+
+    Eu pro minim impedit deleniti, ius veri placerat erroribus ei. Ut cum epicuri principes accommodare, causae ponderum lobortis ex cum. At sit dicit delectus, eu diam summo affert eum. Has cu stet fabellas, quo ea omittam sensibus. At tibique ponderum ullamcorper usu.
+
+    Sit congue quodsi verterem in, tibique deseruisse dissentiunt te est, malis soluta erroribus his ex. Eum in alia putant, eam utinam nusquam at, ne clita quaestio quo. Sit offendit adolescens neglegentur ut. Laoreet qualisque no sed.
+
+    Augue oblique detraxit ei est, qui an habeo invidunt salutandi. Hinc numquam salutatus vim id, nam docendi singulis contentiones et. Et has suscipit legendos, cum paulo theophrastus ut. An zril denique appetere duo, putant consectetuer consequuntur vim ad, senserit argumentum mei eu. Commune vulputate ne mel. Magna facilisis mea cu, tamquam alienum quaestio nec te.
+
+    Nibh invidunt corrumpit eu nam. Usu minim ornatus delicatissimi at, his id etiam graece voluptaria, te sed omnes delicata consectetuer. Vix ei audire inermis, mel ne alii wisi etiam, verear legendos facilisis eos eu. Eum an commodo vidisse mediocrem. His civibus appareat expetenda cu, voluptua praesent intellegam in nec. Id pro nonumy aliquip, eu inermis urbanitas mei.
+`;
