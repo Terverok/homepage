186fae5356b2e0e60bcc9701d893ebde193550ef
Marek Tkaczyk <terverok@gmail.com>
2018-05-26 02:23:21 +0200

    TransparentPicture: utility component using svg

---
+++ src/SpecialUtils/TransparentPicture.vue
@@ -0,0 +1,56 @@
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+    props: {
+        width: {
+            type: Number,
+            default: null,
+        },
+        height: {
+            type: Number,
+            default: null,
+        },
+        mask: {
+            type: String,
+            required: true,
+        },
+        picture: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        viewBox() {
+            return  `0 0 ${this.width} ${this.height}`
+        }
+    }
+})
+
+</script>
+
+<template>
+    <svg
+        v-bind:viewBox="viewBox"
+        preserveAspectRatio="xMinYMin"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+        <defs>
+            <mask id="pictureMask">
+                <image
+                    v-bind:xlink:href="mask"
+                    v-bind:width="width"
+                    v-bind:height="height"
+                />
+            </mask>
+        </defs>
+        <image
+            v-bind:width="width"
+            v-bind:height="height"
+            v-bind:xlink:href="picture"
+            mask="url(#pictureMask)"
+        />
+    </svg>
+</template>
